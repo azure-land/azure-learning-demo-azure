67fe5432326a027f00cbc3a7bbb310c454ff59ff
Gergely Gazso <g.gergely92@gmail.com>
2026-07-21 20:01:53 +0200

    add fekete rogó endpoint

---
+++ src/main/java/com/learning/azure/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.learning.azure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return "Szevasz feketerigó";
+    }
+
+}
